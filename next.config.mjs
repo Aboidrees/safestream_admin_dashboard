cd63597f33d7d4521be9c16bbf3bb0335f9091ad
Muhammad Yousif <m.alshf3ee@gmail.com>
2025-10-04 17:34:23 +0300

    Fix Prisma production engine issue with monorepo plugin

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-  },
+  serverExternalPackages: ['@prisma/client', 'prisma'],
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins.push(new PrismaPlugin())
